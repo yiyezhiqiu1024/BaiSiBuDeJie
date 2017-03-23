4e43384d23bde50c9a0469ee9c03b210260a8d8b
CoderSLZeng <359297567@qq.com>
2017-03-23 22:34:15 +0800

    定义UITabBarController

--- 百思不得姐/百思不得姐/Classes/AppDelegate.m
+++ 百思不得姐/百思不得姐/Classes/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "SLTabBarController.h"
 
 @interface AppDelegate ()
 
@@ -16,59 +17,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    /** 文字属性 **/
-    // 普通状态下的文字属性
-    NSMutableDictionary *normalAttrs = [NSMutableDictionary dictionary];
-    normalAttrs[NSFontAttributeName] = [UIFont systemFontOfSize:14];
-    normalAttrs[NSForegroundColorAttributeName] = [UIColor grayColor];
-    // 选中状态下的文字属性
-    NSMutableDictionary *selectedAttrs = [NSMutableDictionary dictionary];
-    selectedAttrs[NSForegroundColorAttributeName] = [UIColor darkGrayColor];
-    
     // 创建窗口
     self.window = [[UIWindow alloc] init];
     self.window.frame = [UIScreen mainScreen].bounds;
     
     // 设置根控制器
-    UITabBarController *tabBarVc = [[UITabBarController alloc] init];
+    self.window.rootViewController = [[SLTabBarController alloc] init];
     
-    UITableViewController *vc0 = [[UITableViewController alloc] init];
-    vc0.view.backgroundColor = [UIColor redColor];
-    vc0.tabBarItem.title = @"精华";
-    [vc0.tabBarItem setTitleTextAttributes:normalAttrs forState:UIControlStateNormal];
-    [vc0.tabBarItem setTitleTextAttributes:selectedAttrs forState:UIControlStateSelected];
-    vc0.tabBarItem.image = [UIImage imageNamed:@"tabBar_essence_icon"];
-    vc0.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_essence_click_icon"];
-    [tabBarVc addChildViewController:vc0];
-    
-    UIViewController *vc1 = [[UIViewController alloc] init];
-    vc1.view.backgroundColor = [UIColor blueColor];
-    vc1.tabBarItem.title = @"新帖";
-    [vc1.tabBarItem setTitleTextAttributes:normalAttrs forState:UIControlStateNormal];
-    [vc1.tabBarItem setTitleTextAttributes:selectedAttrs forState:UIControlStateSelected];
-    vc1.tabBarItem.image = [UIImage imageNamed:@"tabBar_new_icon"];
-    vc1.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_new_click_icon"];
-    [tabBarVc addChildViewController:vc1];
-    
-    UITableViewController *vc2 = [[UITableViewController alloc] init];
-    vc2.view.backgroundColor = [UIColor greenColor];
-    vc2.tabBarItem.title = @"关注";
-    [vc2.tabBarItem setTitleTextAttributes:normalAttrs forState:UIControlStateNormal];
-    [vc2.tabBarItem setTitleTextAttributes:selectedAttrs forState:UIControlStateSelected];
-    vc2.tabBarItem.image = [UIImage imageNamed:@"tabBar_friendTrends_icon"];
-    vc2.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_friendTrends_click_icon"];
-    [tabBarVc addChildViewController:vc2];
-    
-    UIViewController *vc3 = [[UIViewController alloc] init];
-    vc3.view.backgroundColor = [UIColor grayColor];
-    vc3.tabBarItem.title = @"我";
-    [vc3.tabBarItem setTitleTextAttributes:normalAttrs forState:UIControlStateNormal];
-    [vc3.tabBarItem setTitleTextAttributes:selectedAttrs forState:UIControlStateSelected];
-    vc3.tabBarItem.image = [UIImage imageNamed:@"tabBar_me_icon"];
-    vc3.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_me_click_icon"];
-    [tabBarVc addChildViewController:vc3];
-    
-    self.window.rootViewController = tabBarVc;
+    // 显示窗口
+    [self.window makeKeyAndVisible];
     
     // 显示窗口
     [self.window makeKeyAndVisible];
---
+++ 百思不得姐/百思不得姐/Classes/SLTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  SLTabBarController.h
+//  百思不得姐
+//
+//  Created by Anthony on 17/3/23.
+//  Copyright © 2017年 SLZeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLTabBarController : UITabBarController
+
+@end
