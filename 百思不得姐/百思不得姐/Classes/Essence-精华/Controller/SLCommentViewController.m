47b87fe0ff347c7dd5b2638f5cd15b64bba4e256
CoderSLZeng <359297567@qq.com>
2017-04-02 10:13:09 +0800

    评论控制器界面中cell的显示

--- 百思不得姐/百思不得姐/Classes/Essence-精华/Controller/SLCommentViewController.m
+++ 百思不得姐/百思不得姐/Classes/Essence-精华/Controller/SLCommentViewController.m
@@ -9,28 +9,39 @@
 #import "SLCommentViewController.h"
 
 @interface SLCommentViewController () <UITableViewDelegate>
+/** 工具条底部约束 */
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomMargin;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @end
 
 @implementation SLCommentViewController
 
-#pragma mark - 初始化
+static NSString * const SLTopicCellId = @"topic";
+static NSString * const SLCommentCellId = @"comment";
+
+#pragma mark - 系统回调
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     [self setupBase];
-}
-
-- (void)setupBase
-{
-    self.navigationItem.title = @"评论";
     
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillChangeFrame:) name:UIKeyboardWillChangeFrameNotification object:nil];
 }
 
 - (void)dealloc
 {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+#pragma mark - 设置UI
+- (void)setupBase
+{
+    self.navigationItem.title = @"评论";
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillChangeFrame:) name:UIKeyboardWillChangeFrameNotification object:nil];
+    
+    // 注册cell
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:SLTopicCellId];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:SLCommentCellId];
 }
 
 #pragma mark - 监听
@@ -54,7 +65,46 @@
     }];
 }
 
+#pragma mark - 数据源方法
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 3;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (section == 0) return 1;
+    if (section == 1) return 4;
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.section == 0) {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:SLTopicCellId];
+        cell.textLabel.text = @"最顶部的帖子数据";
+        return cell;
+    } else {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:SLCommentCellId];
+        cell.textLabel.text = [NSString stringWithFormat:@"评论数据 - %zd-%zd", indexPath.section, indexPath.row];
+        return cell;
+    }
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    if (section == 0) return nil;
+    if (section == 1) return @"最热评论";
+    return @"最新评论";
+}
+
 #pragma mark - <UITableViewDelegate>
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.section == 0) return 200;
+    return 44;
+}
+
 /**
  *  当用户开始拖拽scrollView就会调用一次
  */
