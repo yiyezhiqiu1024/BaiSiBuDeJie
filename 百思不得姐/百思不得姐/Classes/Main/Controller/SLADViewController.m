51b1dd5784df387fb33b4944548f1cdccb8d0d09
CoderSLZeng <359297567@qq.com>
2017-04-06 11:42:38 +0800

    1.加载广告界面数据 2.响应头content-type: text/html"报错处理：AFJSONResponseSerializer.m文件中 self.acceptableContentTypes 添加@"text/html"

--- 百思不得姐/百思不得姐/Classes/Main/Controller/SLADViewController.m
+++ 百思不得姐/百思不得姐/Classes/Main/Controller/SLADViewController.m
@@ -7,6 +7,15 @@
 //  广告
 
 #import "SLADViewController.h"
+#import <AFNetworking.h>
+
+/*
+ 1.广告业务逻辑
+ 2.占位视图思想:有个控件不确定尺寸,但是层次结构已经确定,就可以使用占位视图思想
+ 3.屏幕适配.通过屏幕高度判断
+ */
+
+#define code2 @"phcqnauGuHYkFMRquANhmgN_IauBThfqmgKsUARhIWdGULPxnz3vndtkQW08nau_I1Y1P1Rhmhwz5Hb8nBuL5HDknWRhTA_qmvqVQhGGUhI_py4MQhF1TvChmgKY5H6hmyPW5RFRHzuET1dGULnhuAN85HchUy7s5HDhIywGujY3P1n3mWb1PvDLnvF-Pyf4mHR4nyRvmWPBmhwBPjcLPyfsPHT3uWm4FMPLpHYkFh7sTA-b5yRzPj6sPvRdFhPdTWYsFMKzuykEmyfqnauGuAu95Rnsnbfknbm1QHnkwW6VPjujnBdKfWD1QHnsnbRsnHwKfYwAwiu9mLfqHbD_H70hTv6qnHn1PauVmynqnjclnj0lnj0lnj0lnj0lnj0hThYqniuVujYkFhkC5HRvnB3dFh7spyfqnW0srj64nBu9TjYsFMub5HDhTZFEujdzTLK_mgPCFMP85Rnsnbfknbm1QHnkwW6VPjujnBdKfWD1QHnsnbRsnHwKfYwAwiuBnHfdnjD4rjnvPWYkFh7sTZu-TWY1QW68nBuWUHYdnHchIAYqPHDzFhqsmyPGIZbqniuYThuYTjd1uAVxnz3vnzu9IjYzFh6qP1RsFMws5y-fpAq8uHT_nBuYmycqnau1IjYkPjRsnHb3n1mvnHDkQWD4niuVmybqniu1uy3qwD-HQDFKHakHHNn_HR7fQ7uDQ7PcHzkHiR3_RYqNQD7jfzkPiRn_wdKHQDP5HikPfRb_fNc_NbwPQDdRHzkDiNchTvwW5HnvPj0zQWndnHRvnBsdPWb4ri3kPW0kPHmhmLnqPH6LP1ndm1-WPyDvnHKBrAw9nju9PHIhmH9WmH6zrjRhTv7_5iu85HDhTvd15HDhTLTqP1RsFh4ETjYYPW0sPzuVuyYqn1mYnjc8nWbvrjTdQjRvrHb4QWDvnjDdPBuk5yRzPj6sPvRdgvPsTBu_my4bTvP9TARqnam"
 
 @interface SLADViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *launchImageView;
@@ -15,10 +24,40 @@
 
 @implementation SLADViewController
 
+#pragma mark - 系统回调
 - (void)viewDidLoad {
     [super viewDidLoad];
     // 设置启动图片
     [self setupLaunchImage];
+    
+    // 加载广告数据 => 拿到活时间 => 服务器 => 查看接口文档 1.判断接口对不对 2.解析数据(w_picurl,ori_curl:跳转到广告界面,w,h) => 请求数据(AFN)
+    [self loadAdData];
+    
+}
+
+#pragma mark - 设置UI
+- (void)loadAdData
+{
+    
+    // unacceptable content-type: text/html"  响应头
+    
+    // 1.创建请求会话管理者
+    AFHTTPSessionManager *mgr = [AFHTTPSessionManager manager];
+    
+    // 2.拼接参数
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    parameters[@"code2"] = code2;
+    
+    // 3.发送请求
+    [mgr GET:@"http://mobads.baidu.com/cpro/ui/mads.php" parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        // 请求数据 -> 解析数据 -> 展示数据
+        
+        NSLog(@"%@",responseObject);
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"%@",error);
+    }];
     
 }
 
